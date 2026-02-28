dc99b8aa3521f1e72506683188d888ffe6c166c5
Dallas Lendarie <dallaslendaris@gmail.com>
2026-02-27 21:29:08 -0800

    More Updates

--- aidApplication.java
+++ aidApplication.java
@@ -1,19 +1,18 @@
 import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.time.temporal.ChronoUnit;
 import java.util.Locale;
 import java.util.Scanner;
-import java.time.temporal.ChronoUnit;
 
 public class aidApplication {
-    private boolean appStatus;
+
 
     public aidApplication(){
-        this.appStatus = false;
     }
 
-    public boolean ageCheck(){
-        try (Scanner scanner = new Scanner(System.in)) {
+    public boolean ageCheck(Scanner scanner){
+       
             while (true) {
                 System.out.print("Enter your age: ");
                 String input = scanner.nextLine().trim();
@@ -21,20 +20,23 @@ public class aidApplication {
                 try {
                     int age = Integer.parseInt(input);
                     if (age < 0) {
-                        System.out.println("Age cannot be negative. Try again.");
+                        System.out.println("Age cannot be negative. Try again...");
                         continue;
                     }
                     return age >= 18 && age <= 24;
                     
                 } catch (NumberFormatException e) {
-                    System.out.println("Please enter a valid whole number.");
+                    System.out.println("Please enter a valid whole number...");
                 }
             }
-        }
+        
+    }
+    public boolean ynCheck(String input){
+        return !(input.toUpperCase().equals("Y") || input.toUpperCase().equals("N"));
     }
 
-    public boolean residency(){
-        try (Scanner scanner = new Scanner(System.in)) {
+    public boolean residency(Scanner scanner){
+        
                 DateTimeFormatter fmt = DateTimeFormatter.ofPattern("M/uuuu", Locale.US);
                 boolean qindex = true;
                 //
@@ -45,16 +47,19 @@ public class aidApplication {
                 // the user passes the residency portion
                 //
                 while(qindex){
-                    System.out.print("Do you live in California? Y/N");
+                    System.out.print("Do you live in California? (Y/N): ");
                     String input = scanner.nextLine().trim();
-                    if(input.toUpperCase().equals("Y")){
+                    if(ynCheck(input)){
+                        System.out.println("Enter Y or N as your response...");
+                    }
+                    else if(input.toUpperCase().equals("Y")){
                         System.out.print("When did you start living in California? (MM/YYYY): ");
                         String start = scanner.nextLine().trim();
 
                         try {
                             YearMonth moveIn = YearMonth.parse(start, fmt);
                             YearMonth twoYearsAgo = YearMonth.now().minusYears(2);
-                            if(moveIn.isAfter(twoYearsAgo)){
+                            if(moveIn.isBefore(twoYearsAgo)){
                                 return true;
                             }
                             else{
@@ -64,19 +69,24 @@ public class aidApplication {
                             System.out.println("Invalid format. Please use MM/YYYY (example: 02/2024).");
                         }
                     }
+                    else{qindex = false;}
                 }
              
                 //
                 // Ask the user...
-                // 1. Have you ever worked in California
+                // 1. Have you ever worked in California? (Y/N)
                 // 2. What was your start date?
                 // If the user has worked in California for longer 6 months or longer,
                 // the user passes the residency portion
+                //
                 qindex = true;
                 while(qindex){
-                    System.out.print("Have you ever worked in California? Y/N");
+                    System.out.print("Have you ever worked in California? (Y/N): ");
                     String input = scanner.nextLine().trim();
-                    if(input.toUpperCase().equals("Y")){
+                    if(ynCheck(input)){
+                        System.out.println("Enter Y or N as your response...");
+                    }
+                    else if(input.toUpperCase().equals("Y")){
                         System.out.print("What was your start date? (MM/YYYY)");
                         String start = scanner.nextLine().trim();
                         System.out.print("What was your end date? (MM/YYYY)");
@@ -88,24 +98,114 @@ public class aidApplication {
                             long monthsBetween = ChronoUnit.MONTHS.between(startDate, endDate);
                             if(monthsBetween >= 6){
                                 return true;
-                            }else{
-                                qindex = false;
                             }
+                            else{qindex = false;}
                         } catch (DateTimeParseException e) {
                             System.out.println("Invalid format. Please use MM/YYYY (example: 02/2024).");
                         }
                     }
+                    else{qindex = false;}
                 }
+
+                //
+                // Ask the user...
+                // 1. Do your parents live in California? (Y/N)
+                // 2. When did they start living in California? (MM/YYYY)
+                // If the user's parents have lived in california for over a year,
+                // the user passes the residency portion
+                //
+                qindex = true;
+                while(qindex){
+                    System.out.print("Do your parents live in California? (Y/N): ");
+                    String input = scanner.nextLine().trim();
+                    if(ynCheck(input)){
+                        System.out.println("Enter Y or N as your response");
+                    }
+                    else if(input.toUpperCase().equals("Y")){
+                        System.out.print("When did they start living in California? (MM/YYYY): ");
+                        String start = scanner.nextLine().trim();
+                        
+                        try {
+                            YearMonth startDate = YearMonth.parse(start, fmt);
+                            YearMonth oneYear = YearMonth.now().minusYears(1);
+                            if(startDate.isBefore(oneYear)){
+                                return true;
+                            }else{qindex = false;}
+                        } catch (DateTimeParseException e) {
+                            System.out.println("Invalid format. Please use MM/YYYY (example: 02/2024).");
+                        }
+                    }else{qindex = false;}
+                }
+
+                //
+                // Ask the user...
+                // 1. Have you volunteered for a public cause in California? (Y/N)
+                // 2. Can you provide proof of this public cause? (Y/N)
+                // If the user has participated in public cause and has proof,
+                // the user passes the residency portion
+                //
+                qindex = true;
+                while(qindex){
+                    System.out.print("Have you volunteered for a public cause in California? (Y/N): ");
+                    String input = scanner.nextLine().trim();
+                    if(ynCheck(input)){
+                        System.out.println("Enter Y or N as your response");
+                    }
+                    else if(input.toUpperCase().equals("Y")){
+                        System.out.print("Can you provide proof of this public cause? (Y/N): ");
+                        input = scanner.nextLine().trim();
+                        if(ynCheck(input)){
+                            System.out.println("Enter Y or N as your response");
+                        }
+                        else if(input.toUpperCase().equals("Y")){
+                            return true;
+                        }else{qindex = false;}
+                    }
+                    else{qindex = false;};
+                }
+                return false;
+
+
 
                 
                 
             
         }
-    }
     
+    
+    public boolean deansConsideration(Scanner scanner){
+        
+            boolean loop = true;
+            //
+            // Ask the user...
+            // What is your household income? (per month)
+            // If the household income is less than $5000 per month,
+            // the user passes the Deans Consideration
+            //
+            while(loop){
+                System.out.print("What is your household income? (per month): ");
+                String input = scanner.nextLine().trim();
+                try {
+                    int inputnum = Integer.parseInt(input.trim());
+                    if(inputnum < 5000){
+                        return true;
+                    }else{loop = false;}
+                } catch (NumberFormatException e) {
+                    System.out.println("Not a valid number");
+                }
+            }
+        
+        return false;
+    }
 
     public static void main(String[] args) {
         aidApplication app = new aidApplication();
-        app.ageCheck();
+        try (Scanner scanner = new Scanner(System.in)) {
+            if (app.ageCheck(scanner) && (app.residency(scanner) || app.deansConsideration(scanner))) {
+                System.out.println("Pass");
+            } else {
+                System.out.println("Fail");
+            }
+        }
     }
 }
