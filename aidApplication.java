ce6f8512256b764e536603b5a1f12bf5144527d4
Dallas Lendarie <dallaslendaris@gmail.com>
2026-02-27 20:38:03 -0800

    more updates

--- aidApplication.java
+++ aidApplication.java
@@ -3,6 +3,7 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.Locale;
 import java.util.Scanner;
+import java.time.temporal.ChronoUnit;
 
 public class aidApplication {
     private boolean appStatus;
@@ -35,6 +36,7 @@ public class aidApplication {
     public boolean residency(){
         try (Scanner scanner = new Scanner(System.in)) {
                 DateTimeFormatter fmt = DateTimeFormatter.ofPattern("M/uuuu", Locale.US);
+                boolean qindex = true;
                 //
                 // Ask the user...
                 // 1. Do you live in California
@@ -42,20 +44,56 @@ public class aidApplication {
                 // If the currently lives in California and has been living in California for 2 years,
                 // the user passes the residency portion
                 //
-                System.out.print("Do you live in California? Y/N");
-                String input = scanner.nextLine().trim();
-                if(input.toUpperCase().equals("Y")){
-                    System.out.print("When did you start living in California? (MM/YYYY): ");
-                    String start = scanner.nextLine().trim();
+                while(qindex){
+                    System.out.print("Do you live in California? Y/N");
+                    String input = scanner.nextLine().trim();
+                    if(input.toUpperCase().equals("Y")){
+                        System.out.print("When did you start living in California? (MM/YYYY): ");
+                        String start = scanner.nextLine().trim();
 
-                    try {
-                        YearMonth moveIn = YearMonth.parse(start, fmt);
-                        YearMonth twoYearsAgo = YearMonth.now().minusYears(2);
-                        if(moveIn.isAfter(twoYearsAgo)){
-                            return true;
+                        try {
+                            YearMonth moveIn = YearMonth.parse(start, fmt);
+                            YearMonth twoYearsAgo = YearMonth.now().minusYears(2);
+                            if(moveIn.isAfter(twoYearsAgo)){
+                                return true;
+                            }
+                            else{
+                                qindex = false;
+                            }
+                        } catch (DateTimeParseException e) {
+                            System.out.println("Invalid format. Please use MM/YYYY (example: 02/2024).");
                         }
-                    } catch (DateTimeParseException e) {
-                        System.out.println("Invalid format. Please use MM/YYYY (example: 02/2024).");
+                    }
+                }
+             
+                //
+                // Ask the user...
+                // 1. Have you ever worked in California
+                // 2. What was your start date?
+                // If the user has worked in California for longer 6 months or longer,
+                // the user passes the residency portion
+                qindex = true;
+                while(qindex){
+                    System.out.print("Have you ever worked in California? Y/N");
+                    String input = scanner.nextLine().trim();
+                    if(input.toUpperCase().equals("Y")){
+                        System.out.print("What was your start date? (MM/YYYY)");
+                        String start = scanner.nextLine().trim();
+                        System.out.print("What was your end date? (MM/YYYY)");
+                        String end = scanner.nextLine().trim();
+
+                        try {
+                            YearMonth startDate = YearMonth.parse(start, fmt);
+                            YearMonth endDate = YearMonth.parse(end, fmt);
+                            long monthsBetween = ChronoUnit.MONTHS.between(startDate, endDate);
+                            if(monthsBetween >= 6){
+                                return true;
+                            }else{
+                                qindex = false;
+                            }
+                        } catch (DateTimeParseException e) {
+                            System.out.println("Invalid format. Please use MM/YYYY (example: 02/2024).");
+                        }
                     }
                 }
 
